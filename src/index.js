fb83c08f02039d024a6f4fa22b139ad2bbf191c6
Amit Gangwar <gangwaramit449@gmail.com>
2024-08-07 02:46:50 +0530

    Added Auth0

--- src/index.js
+++ src/index.js
@@ -9,17 +9,26 @@ import "./Resources/fonts/walsheim/GTWalsheimPro-Light.ttf";
 import "./Resources/fonts/walsheim/GTWalsheimPro-Bold.ttf";
 import Footer from "./Components/Footer/Footer"; // Import Footer component
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter for routing
+import { Auth0Provider } from "@auth0/auth0-react"; // Import Auth0Provider for authentication
 import Scroll from "./Components/After/Scroll"; // Import Scroll component
 
 // Create root element for rendering the application
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Render the application wrapped with BrowserRouter
+// Render the application wrapped with Auth0Provider and BrowserRouter
 root.render(
-  <BrowserRouter>
-    <App />
-    {/* <Scroll /> */}
-  </BrowserRouter>
+  <Auth0Provider
+    domain="dev-ox710sb7vz2anbkm.us.auth0.com"
+    clientId="PweQjTPb9YKaZs2rkIBBktZTbcgfxdjl"
+    authorizationParams={{
+      redirect_uri: window.location.origin, // Ensure this matches the allowed callback URL in Auth0
+    }}
+  >
+    <BrowserRouter>
+      <App />
+      <Scroll />
+    </BrowserRouter>
+  </Auth0Provider>
 );
 
 // For measuring performance in your app
